b3e55487f589f8c0b1b3093bf43f27143d826954
VladDizh <vladislav.dizhenin@intel.com>
2021-05-17 16:10:32 +0300

    try

--- RCAN_TrainCode/code/convert.py
+++ RCAN_TrainCode/code/convert.py
@@ -1,6 +1,8 @@
 import io
 import numpy as np
-from model import common
+from model import rcan
+import utility
+
 
 from torch import nn
 import torch.utils.model_zoo as model_zoo
@@ -9,96 +11,20 @@ import torch.onnx
 import torch.nn as nn
 import torch.nn.init as init
 
-class RCAN(nn.Module):
-    def __init__(self, args, conv=common.default_conv):
-        super(RCAN, self).__init__()
-        
-        n_resgroups = args.n_resgroups
-        n_resblocks = args.n_resblocks
-        n_feats = args.n_feats
-        kernel_size = 3
-        reduction = args.reduction 
-        scale = args.scale[0]
-        act = nn.ReLU(True)
-        
-        # RGB mean for DIV2K
-        rgb_mean = (0.4488, 0.4371, 0.4040)
-        rgb_std = (1.0, 1.0, 1.0)
-        self.sub_mean = common.MeanShift(args.rgb_range, rgb_mean, rgb_std)
-        
-        # define head module
-        modules_head = [conv(args.n_colors, n_feats, kernel_size)]
+from option import args
+from trainer import Trainer
 
-        # define body module
-        modules_body = [
-            ResidualGroup(
-                conv, n_feats, kernel_size, reduction, act=act, res_scale=args.res_scale, n_resblocks=n_resblocks) \
-            for _ in range(n_resgroups)]
+torch.manual_seed(args.seed)
+# checkpoint = utility.checkpoint(args)
 
-        modules_body.append(conv(n_feats, n_feats, kernel_size))
+torch_model = rcan.RCAN(args)
 
-        # define tail module
-        modules_tail = [
-            common.Upsampler(conv, scale, n_feats, act=False),
-            conv(n_feats, args.n_colors, kernel_size)]
 
-        self.add_mean = common.MeanShift(args.rgb_range, rgb_mean, rgb_std, 1)
-
-        self.head = nn.Sequential(*modules_head)
-        self.body = nn.Sequential(*modules_body)
-        self.tail = nn.Sequential(*modules_tail)
-
-    def forward(self, x):
-        x = self.sub_mean(x)
-        x = self.head(x)
-
-        res = self.body(x)
-        res += x
-
-        x = self.tail(res)
-        x = self.add_mean(x)
-
-        return x 
-
-    def load_state_dict(self, state_dict, strict=False):
-        own_state = self.state_dict()
-        for name, param in state_dict.items():
-            if name in own_state:
-                if isinstance(param, nn.Parameter):
-                    param = param.data
-                try:
-                    own_state[name].copy_(param)
-                except Exception:
-                    if name.find('tail') >= 0:
-                        print('Replace pre-trained upsampler to new one...')
-                    else:
-                        raise RuntimeError('While copying the parameter named {}, '
-                                           'whose dimensions in the model are {} and '
-                                           'whose dimensions in the checkpoint are {}.'
-                                           .format(name, own_state[name].size(), param.size()))
-            elif strict:
-                if name.find('tail') == -1:
-                    raise KeyError('unexpected key "{}" in state_dict'
-                                   .format(name))
-
-        if strict:
-            missing = set(own_state.keys()) - set(state_dict.keys())
-            if len(missing) > 0:
-                raise KeyError('missing keys in state_dict: "{}"'.format(missing))
-
-torch_model = RCAN
-
-# model_url = '//home/vlad/RCAN/RCAN_TrainCode/code/model/__pycache__/pre_model/RCAN_BIX3.pt'
-# model_url = 'https://drive.google.com/file/d/1VcWxeTN2RX7J4Bmu3502dwHUP9bKkJVS/view?usp=sharing'
 model_url = 'https://www.dropbox.com/s/qm9vc0p0w9i4s0n/models_ECCV2018RCAN.zip?dl=0&file_subpath=%2Fmodels_ECCV2018RCAN%2FRCAN_BIX3.pt'
-batch_size = 1    # just a random number
+batch_size = 1    
 
-# Initialize model with the pretrained weights
 map_location = lambda storage, loc: storage
-if torch.cuda.is_available():
-    map_location = None
-# torch_model.load_state_dict(model_zoo.load_url(model_url, map_location=map_location))
+ 
 torch_model.load
 
-# set the model to inference mode
 torch_model.eval()
